c4d1d58031591a8e49d640fdb6cb541bd52e2db0
Trung Duong <trungdt.cmaxx@gmail.com>
2022-12-15 08:20:03 +0700

    config mongoose mvc

---
+++ src/schemas/author.model.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+
+interface IAuthor {
+
+    name: string
+
+}
+
+
+const authorSchema = new Schema<IAuthor>({
+
+    name: String
+
+})
+
+
+const Author = model<IAuthor>('Author', authorSchema);
+
+
+export { Author };
